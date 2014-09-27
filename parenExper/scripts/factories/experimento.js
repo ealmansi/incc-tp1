92b9c11b7926f242aa1378987175835b4846a51d
Fran <fran.dorr@gmail.com>
2014-09-27 17:07:39 -0300

    Cambio cadenas con dificultades

--- parenExper/scripts/factories/experimento.js
+++ parenExper/scripts/factories/experimento.js
@@ -7,42 +7,66 @@
       inmediato: {
         t_prev: 1000,           // tiempo de espera previo a empezar
         t_expo: 1000,           // tiempo de exposicion al estimulo
-        t_resp: 1000,           // tiempo para responder
+        t_resp: 3000,           // tiempo para responder
         secuencias: [
-          '{()}[[]][[][]]',
-          '([]){[[]]}[]{}',
-          '{(())}{{}}[][]',
-          '[(){}][]({}())',
+          '{[()]}',             // Máx 1 cebolla por nivel (3 niveles), correcto
+          '([{}])',             // correcto
+          '[({)]',              //1 incorrecto
+          '{)[}',               //2 incorrectos
+          '(){[()[]]{}}',       // Máx 2 cebollas por nivel (3 niveles), correcto
+          '(({[]()}())[]',      //1 incorrecto
+          '[]{{()[]}()}',       // correcto
+          '(([[]{}](){}',       //2 incorrectos
+          '{}({()[]()}[]())[]', // Máx 3 cebollas por nivel (3 niveles), correcto
+          '{({()[]()}[]())[]',  //1 incorrecto
+          '{({()[]{}}{}[]{',    //2 incorrectos
+          '()[{()[]()}{()}][]'  //correcto
         ],
       },
 
       consciente: {
         t_prev: 1000,           // tiempo de espera previo a empezar
-        t_expo: 1000,           // tiempo de exposicion al estimulo
-        t_resp: 1000,           // tiempo para responder
+        t_expo: 5000,           // tiempo de exposicion al estimulo
+        t_resp: 3000,           // tiempo para responder
         secuencias: [
-          '{()}[[]][[][]]',
-          '([]){[[]]}[]{}',
-          '{(())}{{}}[][]',
-          '[(){}][]({}())',
+          '{[()]}',             // Máx 1 cebolla por nivel (3 niveles), correcto
+          '[({)]',              //1 incorrecto
+          '([{}])',             // correcto
+          '{)[}',               //2 incorrectos
+          '(){[()[]]{}}',       // Máx 2 cebollas por nivel (3 niveles), correcto
+          '[]{{()[]}()}',       // correcto
+          '(({[]()}())[]',      //1 incorrecto
+          '(([[]{}](){}',       //2 incorrectos
+          '{}({()[]()}[]())[]', // Máx 3 cebollas por nivel (3 niveles), correcto
+          '{({()[]{}}{}[]{',    //2 incorrectos
+          '()[{()[]()}{()}][]',  //correcto
+          '{({()[]()}[]())[]'  //1 incorrecto
         ],
       },
 
       inconsciente: {
         t_prev: 1000,           // tiempo de espera previo a empezar
         t_expo: 1000,           // tiempo de exposicion al estimulo
-        t_resp: 1000,           // tiempo para responder
+        t_resp: 5000,           // tiempo para responder
         secuencias: [
-          '{()}[[]][[][]]',
-          '([]){[[]]}[]{}',
-          '{(())}{{}}[][]',
-          '[(){}][]({}())',
+          '{[()]}',             // Máx 1 cebolla por nivel (3 niveles), correcto
+          '[({)]',              //1 incorrecto
+          '{)[}',               //2 incorrectos
+          '([{}])',             // correcto
+          '(){[()[]]{}}',       // Máx 2 cebollas por nivel (3 niveles), correcto
+          '(({[]()}())[]',      //1 incorrecto
+          '(([[]{}](){}',       //2 incorrectos
+          '[]{{()[]}()}',       // correcto
+          '{}({()[]()}[]())[]', // Máx 3 cebollas por nivel (3 niveles), correcto
+          '{({()[]()}[]())[]',  //1 incorrecto
+          '()[{()[]()}{()}][]',  //correcto
+          '{({()[]{}}{}[]{'   //2 incorrectos
         ],
         n_letras: 2,               // n de tarea distractoria
         t_prev_letras: 1000,       // tiempo de espera antes de la tarea secundaria
-        t_expo_letras: 1000,       // cada letra se muestra 500ms
-        t_resp_letras: 1000,       // la respuesta se da en 2000ms
-        t_buff_letras: 500,       // intervalo despues de responder la ultima letra
+        t_expo_letras: 500,       // cada letra se muestra 500ms
+        t_resp_letras: 2000,       // la respuesta se da en 2000ms
+        t_buff_letras: 2000,       // intervalo despues de responder la ultima letra
         gruposLetras: [
           ['T','V','Z','V','V','R','W','R','T','R','T','T','R','U','T'],// 4 2-back, 2 1-back, 2 3-back
           ['C','F','C','F','Z','H','F','F','O','G','O','P','W','P','P'],// 4 2-back, 2 1-back, 2 3-back 
