5b4d7e356db80aa129e3918b36e4817adb0a20dc
Fran <fran.dorr@gmail.com>
2014-10-18 16:54:12 -0300

    agrego analisis rtas por dificultad

--- datos/cleaner.py
+++ datos/cleaner.py
@@ -7,9 +7,9 @@ from pprint import pprint
 secuencias_inmediato = [
 	{'largo': 18, 'rta_correcta': 'N', 'depth':2},
 	{'largo': 18, 'rta_correcta': 'N', 'depth':2},
+	{'largo': 18, 'rta_correcta': 'S', 'depth':2},
 	{'largo': 18, 'rta_correcta': 'N', 'depth':1},
 	{'largo': 12, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':2},
 	{'largo': 6, 'rta_correcta': 'N', 'depth':3},
 	{'largo': 6, 'rta_correcta': 'S', 'depth':2},
 	{'largo': 6, 'rta_correcta': 'S', 'depth':1},
@@ -83,12 +83,32 @@ secuencias_inconsciente = [
 ]
 
 def procesarSujeto(sujeto):
+	# obtenerRtrasCorrectasInmediato(sujeto)
+	obtenerRtas(sujeto, 6, 2)
+
+def obtenerRtasCorrectasInmediato(sujeto):
 	cant_rtas_correctas_inmediato = 0
 	for rta_tiempo in sujeto['inmediato']['respuestas']:
 		indice, rta, tiempo  = rta_tiempo[0], rta_tiempo[1], rta_tiempo[2]
 		if secuencias_inmediato[indice]['rta_correcta'] == rta:
 			cant_rtas_correctas_inmediato = cant_rtas_correctas_inmediato + 1
 	print(cant_rtas_correctas_inmediato)
+
+def obtenerRtas(sujeto, largo, profundidad):
+	cant_rtas_correctas = 0
+	porcentaje = 0.0
+	for rta in sujeto['inmediato']['respuestas']:
+		indice, rta, tiempo = rta[0], rta[1], rta[2]
+		if (secuencias_inmediato[indice]['largo'] == largo) and (secuencias_inmediato[indice]['depth'] == profundidad):
+			if secuencias_inmediato[indice]['rta_correcta'] == rta:
+				cant_rtas_correctas = cant_rtas_correctas + 1
+	porcentaje = cant_rtas_correctas/6.0
+	print 'Rtas correctas', cant_rtas_correctas 
+	print 'largo: ', largo 
+	print 'profundida: ', profundidad
+	print 'Porcentaje: ', porcentaje*100, '%'
+
+
 
 def main():
 	data = None
@@ -97,6 +117,7 @@ def main():
 	sujetos = OrderedDict(sorted(data['resultados'].items(), key=lambda t: t[0]))
 	for key in sujetos:
 		sujeto = sujetos[key]
+		# pprint(sujeto)
 		procesarSujeto(sujeto)
 
 if __name__ == '__main__':
