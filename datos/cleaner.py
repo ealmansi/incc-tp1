4683c21ab810a190f50147818b0b3f2a36d84750
ealmansi <ealmansi@gmail.com>
2014-10-23 15:55:46 -0300

    cleaner ahora escupe csv

--- datos/cleaner.py
+++ datos/cleaner.py
@@ -6,81 +6,81 @@ from collections import OrderedDict
 from pprint import pprint
 
 secuencias_inmediato = [
-	{'largo': 18, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 12, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 6, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 6, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 6, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 6, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 12, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 6, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 12, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 6, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 12, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 6, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 6, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 12, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 12, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 12, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 6, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 12, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 12, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 12, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 6, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 6, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 6, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 6, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 12, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 6, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 12, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 6, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 12, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 12, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 12, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 6, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 12, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 6, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 12, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 12, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 6, 'rta_correcta': 'N', 'depth':2}
+	{'largo': 18, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 12, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 6, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 6, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 6, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 6, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 12, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 6, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 12, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 6, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 12, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 6, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 6, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 12, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 12, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 12, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 6, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 12, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 12, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 12, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 6, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 6, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 6, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 6, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 12, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 6, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 12, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 6, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 12, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 12, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 12, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 6, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 12, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 6, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 12, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 12, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 6, 'rta_correcta': 'N', 'prof':2}
 ]
 
 secuencias_inconsciente = [
-	{'largo': 18, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':1},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':2},
-	{'largo': 18, 'rta_correcta': 'N', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':3},
-	{'largo': 18, 'rta_correcta': 'S', 'depth':2},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':1},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':2},
+	{'largo': 18, 'rta_correcta': 'N', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':3},
+	{'largo': 18, 'rta_correcta': 'S', 'prof':2},
 ]
 
 
@@ -115,7 +115,7 @@ def procesarSujetosPorLargoYProf(sujetos):
 	
 	for i in xrange(6,19, 6):
 		for j in xrange(1,4):
-			print "Largo: ", i, "Prof: ", j
+			# print "Largo: ", i, "Prof: ", j
 			for s in sujetos:	
 				rtas = (0,0)
 				rtas = obtenerRtas(s, i, j)
@@ -134,30 +134,11 @@ def procesarSujetosInconsciente(sujetos):
 
 def procesarSujetosInconscientePorProf(sujetos):
 	for i in xrange(1,4):
-		print "Prof: ", i
+		# print "Prof: ", i
 		for s in sujetos:	
 			rtas_prof= (0,0)
 			rtas_prof = obtenerRtasInc(s, i)
 			print(rtas_prof) 
-
-def procesarSujeto(sujeto):
-	# Analisis de baja de rendimiento según el largo
-	
-
-
-	# obtenerRtrasCorrectasInmediato(sujeto)
-	# obtenerRtas(sujeto, 6, 2)
-
-	# obtenerRtasCorrectasInmediatoBruto(sujeto)
-	total = 0
-	if(esComputador(sujeto)):
-		for i in xrange(6,19,6):
-			for j in xrange(1,4):
-				# print "Computador largo: ", i, "Depth: ", j	
-				total += obtenerRtas(sujeto, i, j)
-		print total
-		if total>=48:
-			print "Una bestia"
 
 def esComputador(sujeto):
 	return sujeto['encuesta']['estudios'] == {'computacion': True}
@@ -199,7 +180,7 @@ def obtenerRtas(sujeto, largo, profundidad):
 	porcentaje = 0.0
 	for rta in sujeto['inmediato']['respuestas']:
 		indice, rta, tiempo = rta[0], rta[1], rta[2]
-		if (secuencias_inmediato[indice]['largo'] == largo) and (secuencias_inmediato[indice]['depth'] == profundidad):
+		if (secuencias_inmediato[indice]['largo'] == largo) and (secuencias_inmediato[indice]['prof'] == profundidad):
 			t_total += tiempo
 			if secuencias_inmediato[indice]['rta_correcta'] == rta:
 				cant_rtas_correctas = cant_rtas_correctas + 1
@@ -214,111 +195,227 @@ def obtenerRtasInc(sujeto, profundidad):
 	porcentaje = 0.0
 	for rta in sujeto['inconsciente']['respuestas']:
 		indice, rta, tiempo = rta[0], rta[1], rta[2]
-		if (secuencias_inconsciente[indice]['depth'] == profundidad):
+		if (secuencias_inconsciente[indice]['prof'] == profundidad):
 			t_total += tiempo
 			if secuencias_inconsciente[indice]['rta_correcta'] == rta:
 				cant_rtas_correctas = cant_rtas_correctas + 1
 	return cant_rtas_correctas, t_total
 
+profundidades = [1, 2, 3]
+largos = [6, 12, 18]
 
+def imprimirEncabezado():
+	print("Computador", end=";")
+	print("PcjAciertosTotal", end=";")
+	print("PcjAciertosProf1", end=";")
+	print("PcjAciertosProf2", end=";")
+	print("PcjAciertosProf3", end=";")
+	print("PcjAciertosLargo6", end=";")
+	print("PcjAciertosLargo12", end=";")
+	print("PcjAciertosLargo18", end=";")
+	print("TPromTotal", end=";")
+	print("TPromProf1", end=";")
+	print("TPromProf2", end=";")
+	print("TPromProf3", end=";")
+	print("TPromLargo6", end=";")
+	print("TPromLargo12", end=";")
+	print("TPromLargo18", end=";")
+
+def imprimirEsComputador(sujeto):
+	if ('computacion' in sujeto['encuesta']['estudios']
+		and sujeto['encuesta']['estudios']['computacion']):
+		print(1, end=";")
+	else:
+		print(0, end=";")
+
+def imprimirInmPcjAciertosTotal(sujeto):
+	cantRespuestasCorrectas = 0
+	for respuesta in sujeto['inmediato']['respuestas']:
+		indice, respuesta, tiempo = respuesta[0], respuesta[1], respuesta[2]
+		if secuencias_inmediato[indice]['rta_correcta'] == respuesta:
+			cantRespuestasCorrectas += 1
+	porcentaje = float(cantRespuestasCorrectas) / len(sujeto['inmediato']['respuestas'])
+	print(round(porcentaje, 4), end=";")
+
+def imprimirInmPcjAciertosPorProf(sujeto):
+	cantRespuestasCorrectasPorProf = {p:0 for p in profundidades}
+	cantRespuestasPorProf = {p:0 for p in profundidades}
+	for respuesta in sujeto['inmediato']['respuestas']:
+		indice, respuesta, tiempo = respuesta[0], respuesta[1], respuesta[2]
+		if secuencias_inmediato[indice]['rta_correcta'] == respuesta:
+			cantRespuestasCorrectasPorProf[secuencias_inmediato[indice]['prof']] += 1
+		cantRespuestasPorProf[secuencias_inmediato[indice]['prof']] += 1
+	for p in profundidades:
+		porcentaje = float(cantRespuestasCorrectasPorProf[p]) / cantRespuestasPorProf[p]
+		print(round(porcentaje, 4), end=";")
+
+def imprimirInmPcjAciertosPorLargo(sujeto):
+	cantRespuestasCorrectasPorLargo = {l:0 for l in largos}
+	cantRespuestasPorLargo = {l:0 for l in largos}
+	for respuesta in sujeto['inmediato']['respuestas']:
+		indice, respuesta, tiempo = respuesta[0], respuesta[1], respuesta[2]
+		if secuencias_inmediato[indice]['rta_correcta'] == respuesta:
+			cantRespuestasCorrectasPorLargo[secuencias_inmediato[indice]['largo']] += 1
+		cantRespuestasPorLargo[secuencias_inmediato[indice]['largo']] += 1
+	for l in largos:
+		porcentaje = float(cantRespuestasCorrectasPorLargo[l]) / cantRespuestasPorLargo[l]
+		print(round(porcentaje, 4), end=";")
+
+def imprimirInmTPromTotal(sujeto):
+	tAcum = 0
+	for respuesta in sujeto['inmediato']['respuestas']:
+		indice, respuesta, tiempo = respuesta[0], respuesta[1], respuesta[2]
+		tAcum += tiempo
+	tProm = float(tAcum) / len(sujeto['inmediato']['respuestas'])
+	print(round(tProm, 4), end=";")
+
+def imprimirInmTPromPorProf(sujeto):
+	tAcumPorProf = {p:0 for p in profundidades}
+	cantRespuestasPorProf = {p:0 for p in profundidades}
+	for respuesta in sujeto['inmediato']['respuestas']:
+		indice, respuesta, tiempo = respuesta[0], respuesta[1], respuesta[2]
+		tAcumPorProf[secuencias_inmediato[indice]['prof']] += tiempo
+		cantRespuestasPorProf[secuencias_inmediato[indice]['prof']] += 1
+	for p in profundidades:
+		tProm = float(tAcumPorProf[p]) / cantRespuestasPorProf[p]
+		print(round(tProm, 4), end=";")
+
+def imprimirInmTPromPorLargo(sujeto):
+	tAcumPorLargo = {l:0 for l in largos}
+	cantRespuestasPorLargo = {l:0 for l in largos}
+	for respuesta in sujeto['inmediato']['respuestas']:
+		indice, respuesta, tiempo = respuesta[0], respuesta[1], respuesta[2]
+		tAcumPorLargo[secuencias_inmediato[indice]['largo']] += tiempo
+		cantRespuestasPorLargo[secuencias_inmediato[indice]['largo']] += 1
+	for l in largos:
+		tProm = float(tAcumPorLargo[l]) / cantRespuestasPorLargo[l]
+		print(round(tProm, 4), end=";")
+
+def imprimirIncPcjAciertosTotal(sujeto):
+	pass
+
+def imprimirIncPcjAciertosPorProf(sujeto):
+	pass
+
+def imprimirIncTPromTotal(sujeto):
+	pass
+
+def imprimirIncTPromPorProf(sujeto):
+	pass
+
+def procesarSujeto(sujeto):
+	imprimirEsComputador(sujeto)
+	imprimirInmPcjAciertosTotal(sujeto)
+	imprimirInmPcjAciertosPorProf(sujeto)
+	imprimirInmPcjAciertosPorLargo(sujeto)
+	imprimirInmTPromTotal(sujeto)
+	imprimirInmTPromPorProf(sujeto)
+	imprimirInmTPromPorLargo(sujeto)
+	imprimirIncPcjAciertosTotal(sujeto)
+	imprimirIncPcjAciertosPorProf(sujeto)
+	imprimirIncTPromTotal(sujeto)
+	imprimirIncTPromPorProf(sujeto)
+	print()
 
 def main():
 	data = None
 	with open('incc-tp1-export-16oct.json') as data_file:
 		data = json.load(data_file)
 	sujetos = OrderedDict(sorted(data['resultados'].items(), key=lambda t: t[0]))
-	# print sujetos.keys()
-	values = sujetos.values()
 
-	# Analisis de baja de rendimiento según el largo
-	print "Total sujetos en largos 6, 12, 18 (Rtas Correctas, Tiempo Total)"
-	procesarSujetosPorLargo(values)
+	imprimirEncabezado()
+	for sujeto in sujetos.values():
+		procesarSujeto(sujeto)
 
-	print "\n"
+	# # Analisis de baja de rendimiento según el largo
+	# print "Total sujetos en largos 6, 12, 18 (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosPorLargo(values)
 
-	# Analisis de baja de rendimiento según el largo Comp
-	print "Computadores en largos 6, 12, 18 (Rtas Correctas, Tiempo Total)"
-	procesarSujetosPorLargo(filter(esComputador, values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis de baja de rendimiento según el largo Comp
+	# print "Computadores en largos 6, 12, 18 (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosPorLargo(filter(esComputador, values))
 
-	# Analisis de baja de rendimiento según el largo No Comp
-	print "No computadores en largos 6, 12, 18 (Rtas Correctas, Tiempo Total)"
-	procesarSujetosPorLargo(filter(noEsComputador, values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis de baja de rendimiento según el largo No Comp
+	# print "No computadores en largos 6, 12, 18 (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosPorLargo(filter(noEsComputador, values))
 
-	# Analisis de baja de rendimiento según el profundidad
-	print "Total sujetos en prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
-	procesarSujetosPorProf(values)
+	# print "\n"
 
-	print "\n"
+	# # Analisis de baja de rendimiento según el profundidad
+	# print "Total sujetos en prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosPorProf(values)
 
-	# Analisis de baja de rendimiento según el largo Comp
-	print "Computadores en prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
-	procesarSujetosPorProf(filter(esComputador, values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis de baja de rendimiento según el largo Comp
+	# print "Computadores en prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosPorProf(filter(esComputador, values))
 
-	# Analisis de baja de rendimiento según el largo No Comp
-	print "No computadores prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
-	procesarSujetosPorProf(filter(noEsComputador, values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis de baja de rendimiento según el largo No Comp
+	# print "No computadores prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosPorProf(filter(noEsComputador, values))
 
-	# Analisis por largo y prof total sujetos
-	print "Total sujetos largo 6,12,18 y prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
-	procesarSujetosPorLargoYProf(values)
+	# print "\n"
 
-	print "\n"
+	# # Analisis por largo y prof total sujetos
+	# print "Total sujetos largo 6,12,18 y prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosPorLargoYProf(values)
 
-	# Analisis por largo y prof total computadores
-	print "Computadores largo 6,12,18 y prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
-	procesarSujetosPorLargoYProf(filter(esComputador,values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis por largo y prof total computadores
+	# print "Computadores largo 6,12,18 y prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosPorLargoYProf(filter(esComputador,values))
 
-	# Analisis por largo y prof No Computadores
-	print "No Computadores largo 6,12,18 y prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
-	procesarSujetosPorLargoYProf(filter(noEsComputador, values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis por largo y prof No Computadores
+	# print "No Computadores largo 6,12,18 y prof 1, 2, 3 (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosPorLargoYProf(filter(noEsComputador, values))
 
-	# Analisis total sujetos INCONSCIENTE
-	print "Total Sujetos INCONSCIENTE (Rtas Correctas, Tiempo Total)"
-	procesarSujetosInconsciente(values)
+	# print "\n"
 
-	print "\n"
+	# # Analisis total sujetos INCONSCIENTE
+	# print "Total Sujetos INCONSCIENTE (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosInconsciente(values)
 
-	# Analisis Computadores INCONSCIENTE
-	print "Computadores INCONSCIENTE (Rtas Correctas, Tiempo Total)"
-	procesarSujetosInconsciente(filter(esComputador,values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis Computadores INCONSCIENTE
+	# print "Computadores INCONSCIENTE (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosInconsciente(filter(esComputador,values))
 
-	# Analisis No Computadores INCONSCIENTE
-	print "No Computadores INCONSCIENTE (Rtas Correctas, Tiempo Total)"
-	procesarSujetosInconsciente(filter(noEsComputador,values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis No Computadores INCONSCIENTE
+	# print "No Computadores INCONSCIENTE (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosInconsciente(filter(noEsComputador,values))
 
-	# Analisis total sujetos por prof INCONSCIENTE
-	print "Total sujetos por prof INCONSCIENTE (Rtas Correctas, Tiempo Total)"
-	procesarSujetosInconscientePorProf(values)
+	# print "\n"
 
-	print "\n"
+	# # Analisis total sujetos por prof INCONSCIENTE
+	# print "Total sujetos por prof INCONSCIENTE (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosInconscientePorProf(values)
 
-	# Analisis Computadores por prof INCONSCIENTE
-	print "Computadores por prof INCONSCIENTE (Rtas Correctas, Tiempo Total)"
-	procesarSujetosInconscientePorProf(filter(esComputador, values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis Computadores por prof INCONSCIENTE
+	# print "Computadores por prof INCONSCIENTE (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosInconscientePorProf(filter(esComputador, values))
 
-	# Analisis Computadores por prof INCONSCIENTE
-	print "No Computadores por prof INCONSCIENTE (Rtas Correctas, Tiempo Total)"
-	procesarSujetosInconscientePorProf(filter(noEsComputador, values))
+	# print "\n"
 
-	print "\n"
+	# # Analisis Computadores por prof INCONSCIENTE
+	# print "No Computadores por prof INCONSCIENTE (Rtas Correctas, Tiempo Total)"
+	# procesarSujetosInconscientePorProf(filter(noEsComputador, values))
+
+	# print "\n"
 
 
 if __name__ == '__main__':
